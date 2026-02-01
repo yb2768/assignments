a903abafb4af4ff76686db62c64d795dc0f7eae3
yb2768 <yb2768@nyu.edu>
2026-01-31 20:25:51 -0500

    Add files via upload

--- gettingStarted.py
+++ gettingStarted.py
@@ -29,7 +29,7 @@ def welcome_assignment_answers(question):
         answer = 4
 
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-        answer = 2
+        answer = 3
 
     else:
         answer = None
